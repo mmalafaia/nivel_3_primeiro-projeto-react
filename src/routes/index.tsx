f6c13c0a1b28d601e4a19fefe6f958cdf5c45d19
Manoel Malafaia <mmalafaia@gmail.com>
2020-06-30 18:04:59 -0300

    Styled Components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,4 +4,11 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
-const Routers: React.FC = () => <h1>Hello World</h1>;
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
+);
+
+export default Routes;
